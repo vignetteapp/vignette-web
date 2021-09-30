a723bd0f69a95e3b108dc627b4a55684e84654cf
Jeeho Ahn <io@fosshost.org>
2021-09-30 19:06:58 +0900

    Update seo.tsx

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -14,7 +14,7 @@ const SEO: React.FC<{ title: string; desc: string; path: string }> = ({
       <meta property="og:url" content={`https://vignetteapp.org${path}`} />
       <meta property="og:image" content="/images/banner.jpg" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="theme-color" content="#F10E5A" />
+      <meta name="theme-color" content="#BE58CB" />
     </head>
   )
 }
