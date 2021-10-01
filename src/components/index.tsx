d6b3500196d86e4dcddaade53631328d35e8b0c1
Jeeho Ahn <io@fosshost.org>
2021-10-01 11:32:18 +0900

    feat(add seo): add SEO stuff (#2)
    * feat(add seo): add SEO stuff
    
    * fix(make preview image larger): make the Twitter preview image larger
    
    * feat(add theme color): add theme color
    
    * fix(fix typo): change '_' to - in seo
    
    * Update seo.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,5 +2,5 @@ import Layout from './layout'
 import Button from './button'
 import BottomBar from './bottomBar'
 import PrimaryText from './primaryText'
-
-export { Layout, Button, BottomBar, PrimaryText }
+import SEO from './seo'
+export { Layout, Button, BottomBar, PrimaryText, SEO }
---
+++ src/components/sections/index.tsx
@@ -0,0 +1,3 @@
+import Home from './home'
+import About from './about'
+export { Home, About }
