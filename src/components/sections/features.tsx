16d3dbc908cc932ce66d785ec489218e47ef6239
Edqe14 <configxgt@gmail.com>
2021-10-04 23:49:39 +0700

    improve layout & styling

--- src/components/sections/features.tsx
+++ src/components/sections/features.tsx
@@ -7,12 +7,9 @@ const Features: React.FC = () => (
     className="flex flex-col md:pr-48 md:pl-12 p-6"
     data-sidebar
   >
-    <div className="flex  flex-row font-inter flex-wrap-reverse ">
-      <div
-        className="relative mx-auto md:mr-0 md:ml-20 xl:px-4"
-        style={{ maxWidth: 570 }}
-      >
-        <div className="z-30 absolute top-0 right-0 -translate-y-20 pt-2 translate-x-8 md:block hidden">
+    <div className="flex flex-col-reverse lg:flex-row font-inter items-center">
+      <div className="relative mt-12 md:mr-0 xl:px-4 w-2/3">
+        <div className="z-30 absolute top-0 right-0 -translate-y-20 pt-2 translate-x-12 md:block hidden">
           <Image src="/images/hat.png" width="140" height="140" alt="hat" />
         </div>
 
@@ -26,8 +23,9 @@ const Features: React.FC = () => (
           loop
         />
       </div>
-      <div className=" max-w-2xl md:ml-20 pt-10">
-        <PrimaryText className="text-3xl gradient-tertiary tracking-tight font-bold font-spartan  py-3 w-full ">
+
+      <div className="w-2/3 lg:ml-20">
+        <PrimaryText className="text-3xl gradient-tertiary tracking-tight font-bold font-spartan  py-3">
           Mean and Very lean
         </PrimaryText>
         <p className=" text-2xl spaced-line ">
@@ -36,7 +34,7 @@ const Features: React.FC = () => (
           <span className="font-semibold "> wastes no time</span>, by making the
           application as small and performant as possible.
         </p>
-        <PrimaryText className="text-3xl gradient-tertiary tracking-tight font-bold font-spartan   py-3 w-full  pt-12">
+        <PrimaryText className="text-3xl gradient-tertiary tracking-tight font-bold font-spartan py-3 pt-12">
           Pixel-Perfect Expressions
         </PrimaryText>
         <p className=" text-2xl spaced-line ">
