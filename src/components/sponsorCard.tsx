1bd1620c6c782f0d413a390684a0cc63d88006cc
Geshii <59031302+Geshii@users.noreply.github.com>
2021-10-09 21:12:21 +0200

    fix stuff
    Co-Authored-By: Edqe <34704796+Edqe14@users.noreply.github.com>

--- src/components/sponsorCard.tsx
+++ src/components/sponsorCard.tsx
@@ -1,29 +1,28 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
-const Card2: React.FC<{
+const SponsorCard: React.FC<{
   img: string
   icon?: boolean
   alt?: string
   href?: string
   className?: string
-}> = ({ img, alt, icon, className, href }) => (
+}> = ({ img, alt, className, href }) => (
   <div
     className={`mx-auto w-full max-w-xs rounded-xl bg-white border-gray-100 border shadow-lg px-5 py-4 text-gray-800 ${className}`}
   >
-      <div className="block relative">
-        <a href={href} target="_blank" rel="noopener noreferrer">
-          <Image
-            alt={alt}
-            src={img}
-            height="50"
-            width="256"
-            layout="responsive"
-            className="object-contain"
-          />
+    <div className="block relative">
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <Image
+          alt={alt}
+          src={img}
+          height="50"
+          width="256"
+          layout="responsive"
+          className="object-contain"
+        />
       </a>
-      </div>
+    </div>
   </div>
 )
 
-export default Card2
+export default SponsorCard
