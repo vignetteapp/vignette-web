913c1ce25758024da405f58ee8f33e598560b8cd
Geshi <frisky.thetw@gmail.com>
2021-10-10 12:04:27 +0200

    Sponsors section (#31)
    * start adding sponsors
    
    * sponsor link + new card design wip
    
    * Sponsoring section
    
    * Update sponsors.tsx
    
    * fix stuff
    
    Co-Authored-By: Edqe <34704796+Edqe14@users.noreply.github.com>
    
    * refactor import + change footer links
    
    * add browserstack
    
    * make sponsorcard bigger
    
    * bigger sponsorcard
    
    Co-authored-by: Edqe <34704796+Edqe14@users.noreply.github.com>

---
+++ src/components/sections/sponsors.tsx
@@ -0,0 +1,57 @@
+import { Layout, PrimaryText } from '@/components'
+import Sponsoring from '@/public/sponsoring.json'
+import SponsorCard from '../sponsorCard'
+
+const Sponsors: React.FC = () => (
+  <Layout
+    id="sponsors"
+    className="flex flex-col md:pr-48 md:pl-12 py-12 p-6"
+    data-sidebar
+  >
+    <PrimaryText className="md:text-5xl text-4xl font-semibold font-spartan tracking-tight md:p-3 py-3">
+      Sponsors
+    </PrimaryText>
+
+    <div className="px-4 py-10 grid gap-x-8 gap-y-20 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6">
+      {Sponsoring.map((m, i) => (
+        <div key={i}>
+          <SponsorCard
+            alt={m.name}
+            img={m.avatar}
+            href={m.url}
+            className="h-full transition-transform duration-300 ease-in-out hover:-translate-y-2"
+          />
+        </div>
+      ))}
+    </div>
+
+    <p className="px-4 text-2xl spaced-line">
+      Want to be a sponsor?
+      <br />
+      {` `}
+      <span className="font-semibold ">
+        Consider visiting our{` `}
+        <a href="https://opencollective.com/vignette">
+          <span className="gradient-primary font-semibold text-transparent bg-clip-text">
+            Open Collective
+          </span>
+        </a>
+        {` `}
+        or{` `}
+        <a href="https://github.com/vignetteapp/">
+          <span className="gradient-primary font-semibold text-transparent bg-clip-text">
+            {` `}
+            GitHub{` `}
+          </span>
+        </a>
+        {` `}
+        page
+      </span>
+      .
+      {`
+    `}
+    </p>
+  </Layout>
+)
+
+export default Sponsors
---
+++ src/components/sponsorCard.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+
+const SponsorCard: React.FC<{
+  img: string
+  icon?: boolean
+  alt?: string
+  href?: string
+  className?: string
+}> = ({ img, alt, className, href }) => (
+  <div
+    className={`mx-auto w-full max-w-xs rounded-xl bg-white border-gray-100 border shadow-lg px-5 py-4 text-gray-800 ${className}`}
+  >
+    <div className="block relative">
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <Image
+          alt={alt}
+          src={img}
+          height="150"
+          width="315"
+          layout="responsive"
+          className="object-contain"
+        />
+      </a>
+    </div>
+  </div>
+)
+
+export default SponsorCard
