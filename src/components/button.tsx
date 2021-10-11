2e40229b0b486f2e85abbea884e08d330bb42ae5
Edqe14 <configxgt@gmail.com>
2021-10-11 18:39:21 +0700

    style(button): add hover effect on button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ const Button: React.FC<{
 }> = ({ children, className, disableGradient }) => (
   <button
     type="button"
-    className={`outline-none rounded-md m-2 text-white text-2xl font-medium px-16 py-3 shadow-md ${
+    className={`outline-none rounded-md m-2 text-white text-2xl font-medium px-16 py-3 transition duration-300 ease-in-out hover:shadow-lg shadow-md ${
       !disableGradient && `gradient-secondary animate-gradient`
     } ${className}`}
   >
