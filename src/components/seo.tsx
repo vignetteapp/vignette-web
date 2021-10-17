05ad64a567b21e84ad7c9e998003d6900a7c513f
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2021-10-17 22:20:04 +0700

    fix: add back missing font

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -20,6 +20,8 @@ const SEO: React.FC<{ title: string; desc: string; path: string }> = ({
 
       <link rel="preload" as="image" href="/images/background.webp" />
 
+      <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
+
       {/* Cloudflare Web Analytics */}
       <script
         defer
