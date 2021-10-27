3fe534be291dcddd48d9c390d0e31dc1b258c6d8
Jeeho Ahn <io@fosshost.org>
2021-10-27 10:15:47 +0900

    feat: upgrade to next 12

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['avatars.githubusercontent.com', 'fosshost.org'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  swcMinify: true,
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true,
   },
 }
