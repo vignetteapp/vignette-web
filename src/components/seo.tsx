23407e7978cd289be5e202db46a5ca8a4e821fed
Jeeho Ahn <io@fosshost.org>
2021-11-01 13:31:21 +0900

    Update seo.tsx

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -16,17 +16,18 @@ const SEO: React.FC<{ title: string; desc: string; path: string }> = ({
       <meta property="og:url" content={`https://vignetteapp.org${path}`} />
       <meta property="og:image" content="/images/banner.jpg" />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content=/images/banner.jpg" />
       <meta name="theme-color" content="#BE58CB" />
 
       <link rel="preload" as="image" href="/images/background.webp" />
 
-      <link href="https://rsms.me/inter/inter.css" rel="preload" as="style" />
+      <link href="https://rsms.me/inter/inter.css" rel="preload" as="style" crossorigin/>
       <link
         href="https://rsms.me/inter/font-files/Inter-roman.var.woff2?v=3.19"
         rel="preload"
         as="font"
         type="font/woff2"
-        crossOrigin="true"
+        crossorigin
       />
       <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
 
@@ -35,7 +36,7 @@ const SEO: React.FC<{ title: string; desc: string; path: string }> = ({
         defer
         src="https://static.cloudflareinsights.com/beacon.min.js"
         data-cf-beacon='{"token": "5d442ef7fdc747cab81b66d66bbd9c06"}'
-      ></script>
+      />
     </Head>
   )
 }
