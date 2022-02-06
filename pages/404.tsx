3a21f01cf47b3b78c3915be6593cc74db33152c8
William Horning <w.horning27@gmail.com>
2022-02-06 07:24:13 -0500

    use width

--- pages/404.tsx
+++ pages/404.tsx
@@ -33,7 +33,7 @@ const Custom404: NextPage = () => {
             <br></br>
             <button
               onClick={() => router.back()}
-              className="m-3 rounded-full bg-pinkRed px-11 py-1 font-semibold text-white "
+              className="m-3 w-44 rounded-full bg-pinkRed px-8 py-1 font-semibold text-white "
             >
               Go Back
             </button>
@@ -41,7 +41,7 @@ const Custom404: NextPage = () => {
               passHref={true}
               href="https://www.ikea.com/us/en/p/blahaj-soft-toy-shark-90373590/"
             >
-              <button className="m-3 rounded-full bg-yellow-500 px-8 py-1 font-semibold text-white ">
+              <button className="m-3 w-44 rounded-full bg-yellow-500 px-8 py-1 font-semibold text-white ">
                 Get a BLÅHAJ
               </button>
             </Link>
