c6e6d1e051b32ea58cdbc248fc96425e488fdd51
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-02-14 20:59:01 +0700

    change sponsors to partners

--- components/sections/Sponsors.tsx
+++ components/sections/Sponsors.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const Sponsors = () => (
   <Container className="mt-28 pt-16" id="sponsors">
-    <h2 className="text-center text-3xl font-bold lg:text-4xl">Our Sponsors</h2>
+    <h2 className="text-center text-3xl font-bold lg:text-4xl">Our Partners</h2>
 
     <div className=" mx-auto grid max-w-7xl grid-cols-2 gap-0.5  px-2 py-4 sm:px-6 md:grid-cols-4 lg:py-8 lg:px-8">
       {sponsors.map((sp, i) => (
@@ -49,11 +49,11 @@ const Sponsors = () => (
         </a>
       ))}
       <a href="/contact">
-        <div className="col-span-1 flex h-full bg-gray-50 px-4 dark:bg-zinc-900 dark:text-gray-300 dark:hover:bg-zinc-800 lg:px-8 ">
-          <div className=" flex h-full items-center  font-semibold">
+        <div className=" col-span-1 flex h-full bg-gray-50 py-8 px-8 dark:bg-zinc-900 dark:text-gray-300 dark:hover:bg-zinc-800 lg:px-8 ">
+          <div className="flex h-full items-center  font-semibold">
             <span className="">
               {` `}
-              Become a sponsor
+              Become a partner
               <span className="hidden lg:inline">{` `}today</span>
               {` `}
               <span className="whitespace-nowrap">-&gt;</span>
