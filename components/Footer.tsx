69c889facde309b4f6f310ab0263b60efb22302b
JcdeA <io@fosshost.org>
2022-03-08 13:27:27 +0000

    fix: deploy error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,8 +11,8 @@ const Footer = () => {
       className="mt-16 w-full bg-gray-100 px-8 py-8 text-primary dark:bg-black  lg:mt-32"
     >
       <div className="mx-auto max-w-3xl">
-        <div className="grid grid-cols-2 text-sm xs:text-base lg:grid-cols-4">
-          <ul className="mb-4 gap-3 text-gray-600 dark:text-gray-200">
+        <div className="footer grid grid-cols-2 text-sm lg:grid-cols-4">
+          <ul className="mb-4 gap-3 tracking-[-.01em] text-gray-600 dark:text-gray-200">
             <span className="font-semibold">Vignette</span>
             <li>
               <Link href="/">{t(`home`)}</Link>
@@ -30,7 +30,7 @@ const Footer = () => {
               <Link href="/contact">{t(`contact`)}</Link>
             </li>
           </ul>
-          <ul className="text-gray-600 dark:text-gray-200 xs:mb-4">
+          <ul className="tracking-[-.01em]  text-gray-600 dark:text-gray-200 xs:mb-4">
             <span className="font-semibold">{t(`social`)}</span>
             <li>
               <Link href="https://github.com/vignetteapp">GitHub</Link>
