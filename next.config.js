4fa6f4f135a2414321e1846850a418379770c5c2
Jeeho Ahn <io@fosshost.org>
2022-08-27 10:22:18 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -23,14 +23,7 @@ module.exports = withContentlayer()({
   swcMinify: true,
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
+    
 
     return config
   },
