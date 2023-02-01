b3c62c5c51141c7d8c6c34b89f8db916d1bb4989
Jeeho Ahn <io@vignetteapp.org>
2023-02-01 04:03:52 +0000

    replace references to next/future/image to next/image

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,6 @@
 import type { GetStaticProps, NextPage } from 'next'
 import Link from 'next/link'
-import Image from 'next/future/image'
+import Image from 'next/image'
 import Blahaj from 'public/images/blahaj.jpg'
 import text404 from 'public/images/404text.png'
 
