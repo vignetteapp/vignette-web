985004f331f4aabfa2ec71007838ffb2dbb7c85c
Ayase Minori <chinodesuuu@gmail.com>
2024-04-18 03:42:09 +0000

    Fix duplicate href
    Signed-off-by: Ayase Minori <chinodesuuu@gmail.com>

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -12,11 +12,7 @@ const CustomLink = (props: Props) => {
   const isInternalLink = href && (href.startsWith(`/`) || href.startsWith(`#`))
 
   if (isInternalLink) {
-    return (
-      <Link href={href} {...props}>
-        {props.children}
-      </Link>
-    )
+    return <Link {...props}>{props.children}</Link>
   }
 
   return <a target="_blank" rel="noopener noreferrer" {...props} />
